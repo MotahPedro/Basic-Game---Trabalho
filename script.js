c1dee272cc83af98bcd31099f79d0815fe87ded4
MotahPedro <motahpedro@gmail.com>
2024-09-05 11:05:05 -0300

    feat: hero attack, special and usepotion working properlly

--- script.js
+++ script.js
@@ -1,35 +1,89 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            hero: {life: 100, attack: getRandomAttack(12), mana: 100, name: 'Anna'},
-            villain: {life: 76, attack: getRandomAttack(5), name: 'Goblin'},
+            hero: { life: 100, attack: getRandomAttack(12), mana: 100, name: 'Anna' },
+            heroSprite: 'Assets/Duelista-1-MovingSprite.gif',
+            originalHeroSprite: 'Assets/Duelista-1-MovingSprite.gif',
+            attackHeroGif: 'Assets/Gif-Duelista-1.gif',
+            specialHeroGif: 'Assets/Gif-Duelista-1.gif',
+
+            villain: { life: 100, attack: getRandomAttack(10), name: 'Goblin' },
+            villainSprite: 'Assets/SpearGoblin-sprite.png',
+            originalVillainSprite: 'Assets/SpearGoblin-sprite.png',
+            attackVillainGif: 'Assets/Gif-SpearGoblin.gif',
+
+            isMusicPlaying: false
         }
 
         function getRandomAttack(baseAttack) {
-            const minModifier = 0.9; // 10% less
-            const maxModifier = 1.3; // 30% more
+            const minModifier = 0.9;
+            const maxModifier = 1.3;
             const randomModifier = Math.random() * (maxModifier - minModifier) + minModifier;
             return Math.round(baseAttack * randomModifier);
         }
     },
 
+    mounted() {
+        this.playMusic();
+    },
+
+    computed: {
+        heroLifePercentage() {
+            return Math.max((this.hero.life / 100) * 100, 0);
+        },
+        heroManaPercentage() {
+            return Math.max((this.hero.mana / 100) * 100, 0);
+        },
+        villainLifePercentage() {
+            return Math.max((this.villain.life / 100) * 100, 0);
+        }
+    },
+
     methods: {
-        attack(isHero){
-            this.hero.life -= this.villain.attack;
+        attack(isHero) {
+            const damage = this.hero.attack;
+            this.villain.life = Math.max(this.villain.life - damage, 0);
+
+            this.heroSprite = this.attackHeroGif;
+
+            setTimeout(() => {
+                this.heroSprite = this.originalHeroSprite;  
+            }, 2400);
+            // this.villainAction();
         },
-        defense(isHero){
-            this.villain.life -= this.hero.attack;
+        defense(isHero) {
+            this.hero.life -= this.hero.attack - this.villain.attack;
+            // this.villainAction();
         },
-        usePotion(isHero){
-            this.hero.life += 10;
+        usePotion(isHero) {
+            if(this.hero.life === 100){
+                'You are already at full health!'
+            } else if(this.hero.life >= 80){
+                this.hero.life = 100;
+            } else if(this.hero.life < 80){
+                this.hero.life += 20;
+            } else if (this.hero.life + 20 > 100) {
+                this.hero.life = 100;
+            }
+            // this.villainAction();
         },
-        special(isHero){
-            this.villain.life -= this.hero.attack * 2;
-            this.hero.mana -= 40;
+        special(isHero) {
+            if (this.hero.mana >= 40) {
+                const damage = (this.hero.attack * 2)
+                this.villain.life = Math.max(this.villain.life - damage, 0);
+                this.hero.mana = Math.max(this.hero.mana - 40, 0);
+
+                this.heroSprite = this.specialHeroGif;
+
+                setTimeout(() => {
+                    this.heroSprite = this.originalHeroSprite;
+                }, 4000);
+                // this.villainAction();
+            }
         },
-        flee(isHero){
+        flee(isHero) {
 
         },
 
@@ -37,10 +91,22 @@ createApp({
             const action = ['attack', 'defense', 'usePotion', 'flee'];
             const randomAction = action[Math.floor(Math.random() * action.length)];
             this[randomAction](false);
-        }
+        },
 
-
-
+        playMusic() {
+            const music = document.getElementById("backgroundMusic");
+            if (!this.isMusicPlaying) {
+                music.play();
+                this.isMusicPlaying = true;
+            }
+        },
+        pauseMusic() {
+            const music = document.getElementById("backgroundMusic");
+            if (this.isMusicPlaying) {
+                music.pause();
+                this.isMusicPlaying = false;
+            }
+        },
     }
 
 
